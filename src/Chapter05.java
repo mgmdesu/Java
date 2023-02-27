047b02140467701f59db4d04598af308cd0de07f
megumi <megumi@meguminoair>
2023-02-27 16:22:09 +0900

    [finish]lesson5

---
+++ src/Chapter05.java
@@ -0,0 +1,21 @@
+
+public class Chapter05 {
+	public static void main(String[] args) {
+		long long1 = 9223372036854775807L;
+		System.out.println(long1);
+		double double1 = 1234.5;
+		System.out.println(double1);
+		boolean boolean1 = true;
+		System.out.println(boolean1);
+		
+		Float float1 = 1234.5f;
+		System.out.println(float1);
+		Integer integer1 = 1;
+		System.out.println(integer1);
+		Character character1 = 0x3042;
+		System.out.println(character1);
+		
+		int[] array = {0, 1, 2, 3, 4};
+		System.out.println(array[3]);
+	}
+}
