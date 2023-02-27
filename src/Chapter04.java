f84d846040831430c8b187f4102af889abbbe87b
megumi <megumi@meguminoair>
2023-02-27 15:09:51 +0900

    [finish]lesson4

---
+++ src/Chapter04.java
@@ -0,0 +1,13 @@
+
+public class Chapter04 {
+
+	public static void main(String[] args) {
+		String name;
+		final int LUCKY_NUMBER = 7;
+		name = "Fuji";
+		System.out.println(name);
+		name = "昨日より今日";
+		System.out.println(name);
+		System.out.println(LUCKY_NUMBER);
+	}
+}
---
+++ src/Variable01.java
@@ -0,0 +1,18 @@
+
+public class Variable01 {
+
+	public static void main(String[] args) {
+		int num1; //int型 変数num; 宣言
+		String str1; //string型 変数str1; 宣言
+		num1 = 10; //num1を初期化
+		str1 = "Hello World"; //str1を初期化
+		System.out.println(num1);
+		System.out.println(str1);
+		
+		num1 = 20; //num1に20を再代入している
+		System.out.println(num1);
+		
+		int num2 = 30; //int型 変数num2;宣言＋num2を初期化
+		System.out.println(num2);
+	}
+}
