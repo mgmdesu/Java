2159b4811a2109bba7a5e53956bfb1f8bca6e67e
megumi <megumi@meguminoair>
2023-03-01 17:27:00 +0900

    [finish]lesson8

--- src/Chapter07.java
+++ src/Chapter07.java
@@ -1,6 +1,7 @@
+
 public class Chapter07 {
     public static void main(String[] args) {
-        String color = "yellow"; // 例としてyellowで初期化。他の色でも良い
+        String color = "red"; 
         switch(color) {
           case "red" :
             System.out.println("赤信号です");
--- src/Null01.java
+++ src/Null01.java
@@ -3,11 +3,11 @@ public class Null01 {
 
 	 public static void main(String[] args) {
 		 String string1;
-		 string1 = "あいう";
+		 string1 = "あいう"; //あいうの文字列を出力
 		 System.out.println(string1 + "の文字数：" + string1.length());
 		 
 		 string1 = "";
-		 System.out.println(string1 + "の文字数：" + string1.length());
+	     System.out.println(string1 + "の文字数：" + string1.length());
 		 
 		 string1 = null;
 		 System.out.println(string1 + "の文字数：" + string1.length());
