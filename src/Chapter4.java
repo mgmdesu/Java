e42b1d14c935da2dc72a4c71e632efaa6822098f
mgmdesu <meg928.71j@gmail.com>
2023-03-24 21:56:11 +0900

    [fin]lesson3/4.re

---
+++ src/Chapter4.java
@@ -0,0 +1,14 @@
+
+public class Chapter4 {
+	public static void main(String[] args) {
+		String name = "鈴木";
+		System.out.println(name);
+		
+		final int LUCKY_NUMBER = 7;
+		System.out.println(LUCKY_NUMBER);
+		
+		name = "佐藤";
+		System.out.println(name);
+		
+	}
+}
