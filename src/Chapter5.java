bcadf1231930dd40caebe8deeebc1c221a897af8
mgmdesu <meg928.71j@gmail.com>
2023-03-25 21:17:39 +0900

    [fin]lesson5-re

---
+++ src/Chapter5.java
@@ -0,0 +1,22 @@
+
+public class Chapter5 {
+	public static void main(String[] args) {
+		long number = 9223372036854775806L;
+		System.out.println(number);
+		double num = 3.14;
+		System.out.println(num);
+		boolean i = true;
+		System.out.println(i);
+		
+		Float f = 0.1234f;
+		System.out.println(f);
+		Integer I = 1;
+		System.out.println(I);
+		Character c = 'い';
+		System.out.println(c);
+		
+		int[] n = {1, 2, 3, 4, 5};
+		System.out.println(n[3]);
+	}
+
+}
