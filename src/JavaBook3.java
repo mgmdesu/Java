c059bb9eb3dfea56d0c0e78a015bff5d49dcb128
mgmdesu <meg928.71j@gmail.com>
2023-03-30 23:12:11 +0900

    [start]lesson3

---
+++ src/JavaBook3.java
@@ -0,0 +1,15 @@
+
+public class JavaBook3 {
+	public static void main(String[] args) {
+		//コード3-1,3-3
+		boolean tenki = true;
+		if(tenki == true) {
+			System.out.println("洗濯をします");
+			System.out.println("散歩にいきます");
+		} else {
+			System.out.println("DVDをみます");
+		}
+		
+	}
+
+}
